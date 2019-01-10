5ac20178d52dc9a9a643353c9142f2e28446c709
tree <treeforest456@gmail.com>
2019-01-10 14:45:53 -0800

    added decision tree regressor stuff

--- regression/regression.py
+++ regression/regression.py
@@ -18,3 +18,28 @@ lr_model.summary.rootMeanSquaredError
 
 lr_model.save('lr1.model')
 
+
+# decision tree regressor
+from pyspark.ml.regression import DecisionTreeRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.feature import VectorAssembler
+
+pp_df.take(1)
+vectorAssembler = VectorAssembler(inputCols=['AT', 'V', 'AP', 'RH'], outputCol='features')
+vpp_df = vectorAssembler.transform(pp_df)
+vpp_df.take(1)
+
+splits = vpp_df.randomSplit([0.7, 0.3])
+train_df = splits[0]
+test_df = splits[1]
+train_df.count()
+test_df.count()
+vpp_df.count()
+
+dt = DecisionTreeRegressor(featuresCol='features', labelCol='PE')
+dt_model = dt.fit(train_df)
+dt_predictions = dt_model.transform(test_df)
+
+dt_evaluator = RegressionEvaluator(labelCol='PE', predictionCol='prediction', metricName='rmse')
+rmse = dt_evaluator.evaluate(dt_predictions)
+
